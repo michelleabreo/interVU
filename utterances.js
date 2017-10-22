ce4fd9870ae068c5f22e41242a5bc19be1a1f844
Michelle Abreo <michelle.e.abreo@vanderbilt.edu>
2017-10-22 10:41:29 -0500

    Add files via upload

---
+++ utterances.js
@@ -0,0 +1,38 @@
+{
+    "intents": [
+      {
+          "name": "AMAZON.CancelIntent",
+          "samples": []
+      },
+      {
+          "name": "AMAZON.HelpIntent",
+          "samples": []
+      },
+      {
+          "name": "AMAZON.StopIntent",
+          "samples": []
+      },
+      {
+          "name": "GetNewQuestionIntent",
+          "samples": [
+            "ask me a question",
+            "ask me a behavioral question",
+            "hit me up with a question",
+            "ask away",
+            "hit me up",
+            "ask me something",
+            "ask me something please",
+            "please ask me something",
+            "ask me a question please",
+            "ask",
+            "ask me another",
+            "ask me a different question",
+            "ask me another question",
+            "new question",
+            "new one",
+            "different one please"
+          ],
+          "slots": []
+      }
+    ]
+}
